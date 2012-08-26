8f588c219b804552fbf32568fb96a5c112061800
Soheil <ysoheil@gmail.com>
2012-08-25 22:04:55 -0700

    enahnce scripts

--- make
+++ make
@@ -2,4 +2,5 @@ echo -e "#!/bin/bash\n" > Lock\ App.app/Contents/MacOS/run.sh
 echo -e "echo '`cat pool.sh|base64`'|base64 -D> ~/pool.sh\n" >> Lock\ App.app/Contents/MacOS/run.sh
 cat Lock\ App.app/Contents/MacOS/run.src >> Lock\ App.app/Contents/MacOS/run.sh
 hdiutil create LockApp.dmg -srcfolder Lock\ App.app/ -ov
+exit
 scp LockApp.dmg root@pubbay.com:/home/content/pub/lockapp
--- pool.sh
+++ pool.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+kill `ps aux | grep pool.sh | awk '{print $2}' | grep -v $$`
+clear
 while true
 do
- if [ ! -f "code" ]
+ if [ ! -f ~/.lock-app-code ]
  then
   echo "Please open up the Lock App on your iOS device and enter the code displayed:"
   read code
-  echo $code > code
+  echo $code > ~/.lock-app-code
+  echo "Please enter your Mac password to unlock your account:"
+  read pass
+  echo $pass | base64 > ~/.lock-app-pass
  fi
  resp=$(curl -s "http://pubbay.com/lockapp/?admin&uuid=$code")
  echo $resp
  if [ "$resp" = "lock" ]
  then
-  `/Users/soheil/lock`
-  `say 'lock'`
+  open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
  fi
  if [ "$resp" = "unlock" ]
  then
-  `sh key`
-  `say 'unlock'`
+  osascript -e 'tell application "System Events" to keystroke "\n"'
+  osascript\
+ -e 'set ok to do shell script "cat ~/.lock-app-pass|base64 -D"'\
+ -e 'tell application "System Events" to keystroke ok'
+  osascript -e 'tell application "System Events" to keystroke return'
  fi
  sleep 1
 done
