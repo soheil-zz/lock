3efc103a7c8d5dbab09f6035268b0a884c763bcb
Soheil <ysoheil@gmail.com>
2017-09-27 20:49:06 -0700

    js

--- init.js
+++ init.js
@@ -1,1 +1,3 @@
-document.querySelector('#hireme').innerHTML = '<a href="https://getcruise.com/careers"><img width="300" src="https://raw.githubusercontent.com/soheil/lock/master/i/c.jpg"></a>'
+setTimeout(function() {
+document.getElementById('hireme').innerHTML = '<a href="https://getcruise.com/careers"><img width="300" src="https://raw.githubusercontent.com/soheil/lock/master/i/c.jpg"></a>'
+}, 1000)
